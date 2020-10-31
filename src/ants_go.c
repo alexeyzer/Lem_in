8768b843e5b2dcb8ee2c4ce3c393aa4f16893606
Alexa Guillermina <aguiller@mi-m1.msk.21-school.ru>
2020-10-31 15:07:40 +0300

    add

--- src/ants_go.c
+++ src/ants_go.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ants_go.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andrew <andrew@student.42.fr>              +#+  +:+       +#+        */
+/*   By: aguiller <aguiller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 11:49:16 by andrew            #+#    #+#             */
-/*   Updated: 2020/10/30 14:13:23 by andrew           ###   ########.fr       */
+/*   Updated: 2020/10/31 15:04:08 by aguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,16 +70,62 @@ void	move_ants(t_lemin *lemin, t_path *path)
 			//	ft_printf("ant=%d, path =%s,  prev->ant=%d, prec->path=%s\n", current->ant, find_room(lemin->rooms, current->to), current->next->ant, find_room(lemin->rooms, current->next->to));
 			if (current->ant && !current->next->ant && (!current->prev || current->prev->ant))
 				push(current, lemin);
-			getchar();
 			if(current->ant && current->next->ant && !current->next->next)
 				push(current, lemin);
-			getchar();
 			//printf("b=%d, f=%d\n", ant_scan_b(current), ant_scan_f(current));
 			if(!ant_scan_b(current) && !current->next->ant && ant_scan_f(current))
 				push(current, lemin);
-			getchar();
 			current = current->next;
 		}
+	}
+}
+
+t_path *getpathbyparam(t_path *path, int to, int from)
+{
+	while (path)
+	{
+		if (path->from == from && path->to == to)
+			return (path);
+		path = path->next;
+	}
+	return (NULL);
+}
+
+t_path *getlustpath(t_path *path)
+{
+	t_path *now;
+	int to;
+	int from;
+	
+	now = path;
+	while (now != NULL)
+	{
+		if (now->ant && now->next != NULL)
+		{
+			to = now->to;
+			from = now->from;
+		}
+		now = now->next;
+	}
+	now = getpathbyparam(path, to ,from);
+	return (now);
+}
+
+void	move_ants_second(t_lemin *lemin, t_path *path)
+{
+	t_path	*current;
+
+	current = getlustpath(path);
+	while (current != NULL)
+	{
+		if (current->ant && current->next)
+		{
+			if (!current->next->ant)
+				push(current, lemin);
+		}
+		else if (current->ant)
+			push(current, lemin);
+		current = current->prev;
 	}
 }
 
@@ -92,17 +138,17 @@ void print_path(t_path *p)
 	}
 }
 
-void 	ant_to_path(t_lemin *lemin, t_paths *path, int ant)
+void 	ant_to_path(t_lemin *lemin, t_paths *paths, int ant)
 {
 	char 	*room;
 	
-	move_ants(lemin, path->headpath);
-	if (path->ants_go && !path->headpath->ant)
+	move_ants(lemin, paths->headpath);
+	if (paths->ants_go && !paths->headpath->ant)
 	{
-		path->headpath->ant = ant;
-		room = find_room(lemin->rooms, path->headpath->to);
+		paths->headpath->ant = ant;
+		room = find_room(lemin->rooms, paths->headpath->to);
 		ft_printf("L%d-%s ", ant, room);
-		path->ants_go--;
+		paths->ants_go--;
 	}
 }
 
@@ -126,11 +172,23 @@ int		is_path_clear(t_paths *paths)
 	return(1);
 }
 
+int isallpathsclear(t_paths *headpaths)
+{
+	int result;
+
+	result = 0;
+	while (headpaths != NULL)
+	{
+		result = result + is_path_clear(headpaths);
+		headpaths = headpaths->next;
+	}
+	return (result);
+}
+
 void 	ants_go(t_lemin *lemin)
 {
 	int		ant;
 	t_paths *paths;
-	t_path	*path;
 	
 	paths = lemin->bestsolution->headpaths;
 	ant = 1;
@@ -144,18 +202,14 @@ void 	ants_go(t_lemin *lemin)
 		ft_printf("\n");
 		paths = lemin->bestsolution->headpaths;
 	}
-	while (!is_path_clear(paths))
+	while (!isallpathsclear(paths))
 	{
-		path = paths->headpath;
-		while(path->next)
+		paths = lemin->bestsolution->headpaths;
+		while (paths != NULL)
 		{
-			//print_path(paths->headpath);
-			//getchar();
-			move_ants(lemin, path);
-			path = path->next;
-			ft_printf("\n");
-			getchar();
+			move_ants_second(lemin, paths->headpath);
+			paths = paths->next;
 		}
-		paths = paths->next;
+		ft_printf("\n");
 	}
 }
